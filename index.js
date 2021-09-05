6c133884c18e13f6b32c01c44ca725226dbf4dbf
Alvin <yansonalvin29@gmail.com>
2021-09-05 14:58:07 +0800

    testing connection

---
+++ index.js
@@ -0,0 +1,17 @@
+const Web3 = require('web3');
+
+const customProvider = {
+    sendAsync: (payload, cb) => {
+        console.log('you called')
+        console.log(payload);
+        cb(undefined, 100)
+    }
+}
+
+// const provider = new Web3.providers.HttpProvider('http://localhost:7545');
+const web3 = new Web3(customProvider);
+// connected to the blockchain
+
+web3.eth.getBlockNumber().then(() => {
+    console.log('done!!')
+})
